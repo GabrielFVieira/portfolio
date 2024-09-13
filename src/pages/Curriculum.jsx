7bc9929575e4371dfc780fc64f299d70220ca5f3
Gabriel Figueiredo <gabrielfigueiredov@gmail.com>
2024-09-13 01:38:56 -0300

    Added curriculum pdf

--- src/pages/Curriculum.jsx
+++ src/pages/Curriculum.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import { getFilePlugin } from '@react-pdf-viewer/get-file';
 
 import pageCSS from './page.module.css';
-import css from './curriculum.module.css';
-
-import curriculum from '../assets/images/curriculum.jpg';
+import '@react-pdf-viewer/core/lib/styles/index.css';
 
 export default function Curriculum() {
+	const getFilePluginInstance = getFilePlugin();
+	const { Download } = getFilePluginInstance;
+
 	return (
 		<div className={pageCSS.pageContainer}>
 			<Helmet title="Currículo" />
-			<div className={pageCSS.pageContent}>
-				<img className={css.curriculum} src={curriculum} alt="Curriculum" />
-			</div>
+			{/* <Download>
+			{
+				(props) => (
+					<button onClick={props.onClick}>Download</button>
+				)
+			}
+			</Download> */}
+			<Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+				<Viewer plugins={[getFilePluginInstance]} fileUrl='/portfolio/curriculum.pdf' />
+				{/* <div className={pageCSS.pageContent}>
+					<img className={css.curriculum} src={curriculum} alt="Curriculum" />
+				</div> */}
+			</Worker>
 		</div>
 	);
 }
